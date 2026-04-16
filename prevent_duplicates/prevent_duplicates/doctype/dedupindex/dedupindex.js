88cea9dc062a20075bf72513a65d8f5f28cad723
iron3oxide <hello@iron3oxi.de>
2026-04-16 00:15:03 +0000

    initial commit

---
+++ prevent_duplicates/prevent_duplicates/doctype/dedupindex/dedupindex.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, iron3oxide and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("DedupIndex", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ prevent_duplicates/prevent_duplicates/doctype/dedupindexdocfield/dedupindexdocfield.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, iron3oxide and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class DedupIndexDocField(Document):
+	pass
